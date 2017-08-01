12eb44836417c83565ff4164633c32125676bd90
dev <dev@plutod2d.com>
2017-08-01 16:24:19 -0700

    update stage1.employment.EmploymentByState via pluto

--- src/main/python/stage1/employment.py
+++ src/main/python/stage1/employment.py
@@ -1,3 +1,12 @@
+###---GLOBAL_IMPORTS_START---###
+from smv import *
+from pyspark.sql.functions import *
+###---GLOBAL_IMPORTS_END---###
+###---EmploymentByState_IMPORTS_START---###
+import stage1.inputdata
+
+
+###---EmploymentByState_IMPORTS_END---###
 from smv import *
 from pyspark.sql.functions import col, sum, lit
 
@@ -6,11 +15,15 @@ from stage1 import inputdata
 __all__ = ['EmploymentByState']
 
 class EmploymentByState(SmvModule, SmvOutput):
-    """Python ETL Example: employ by state"""
+    """
+    Python ETL Example: employ by state
+    """
 
     def requiresDS(self):
-        return [inputdata.Employment]
+        return [stage1.inputdata.Employment]
 
     def run(self, i):
         df = i[inputdata.Employment]
-        return df.groupBy(col("ST")).agg(sum(col("EMP")).alias("EMP"))
+        return df.groupBy(col("ST")).agg(sum(col("EMP")).alias("PME"))
+    def isEphemeral(self):
+        return False
