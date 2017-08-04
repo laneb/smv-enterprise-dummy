d057961605d12d28170cea9aac857db693a0eca8
dev <dev@plutod2d.com>
2017-08-04 00:35:55 +0000

    update stage1.employment.EmploymentByState via pluto

--- src/main/python/stage1/employment.py
+++ src/main/python/stage1/employment.py
@@ -24,6 +24,6 @@ class EmploymentByState(SmvModule, SmvOutput):
 
     def run(self, i):
         df = i[inputdata.Employment]
-        return df.groupBy(col("ST")).agg(sum(col("EMP")).alias("LMN"))
+        return df.groupBy(col("ST")).agg(sum(col("EMP")).alias("mnop"))
     def isEphemeral(self):
         return False
